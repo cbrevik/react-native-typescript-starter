d1008c0d5add9b5baf9d3ef1fb50c55e897bbefe
Christian Brevik <christian.brevik@gmail.com>
2017-05-29 11:08:47 +0200

    Remove getProjectRoots with rc2

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -2,9 +2,6 @@ module.exports = {
     getTransformModulePath() {
         return require.resolve('react-native-typescript-transformer')
     },
-    getProjectRoots() {
-        return [__dirname];
-    },
     getSourceExts() {
         return ['ts', 'tsx'];
     }
